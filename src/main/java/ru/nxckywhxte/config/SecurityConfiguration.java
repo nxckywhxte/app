517346f48054b9cff0cbdcebdac5b9db8c04b518
Nikita Belonogov <crcku.mark00@gmail.com>
2023-04-22 17:26:41 +0300

    init

--- src/main/java/ru/nxckywhxte/config/SecurityConfiguration.java
+++ src/main/java/ru/nxckywhxte/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package ru.nxckywhxte.config;
 
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
